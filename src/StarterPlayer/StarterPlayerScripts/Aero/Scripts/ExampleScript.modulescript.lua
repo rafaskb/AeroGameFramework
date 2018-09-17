a565b1b65db8cf77c0a1f9c9ed7676b1cc82073f
rafaskb <rafa.skoberg@hotmail.com>
2018-09-17 15:34:32 -0300

    Create example scripts.

---
+++ src/StarterPlayer/StarterPlayerScripts/Aero/Scripts/ExampleScript.modulescript.lua
@@ -0,0 +1,11 @@
+local ExampleScript = {}
+
+function ExampleScript:Start()
+    print("[Client] [ExampleScript] Start")
+end
+
+function ExampleScript:Init()
+    print("[Client] [ExampleScript] Init")
+end
+
+return ExampleScript
