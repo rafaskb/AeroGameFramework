76f176368385e244b451dcd661a3581b4b3ea055
rafaskb <rafa.skoberg@hotmail.com>
2018-09-17 16:36:14 -0300

    Remove prints from ExampleScripts.

--- src/ServerStorage/Aero/Scripts/ExampleScript.modulescript.lua
+++ src/ServerStorage/Aero/Scripts/ExampleScript.modulescript.lua
@@ -1,11 +1,9 @@
 local ExampleScript = {}
 
 function ExampleScript:Start()
-    print("[Server] [ExampleScript] Start")
 end
 
 function ExampleScript:Init()
-    print("[Server] [ExampleScript] Init")
 end
 
 return ExampleScript
--- src/StarterPlayer/StarterPlayerScripts/Aero/Scripts/ExampleScript.modulescript.lua
+++ src/StarterPlayer/StarterPlayerScripts/Aero/Scripts/ExampleScript.modulescript.lua
@@ -1,11 +1,9 @@
 local ExampleScript = {}
 
 function ExampleScript:Start()
-    print("[Client] [ExampleScript] Start")
 end
 
 function ExampleScript:Init()
-    print("[Client] [ExampleScript] Init")
 end
 
 return ExampleScript
