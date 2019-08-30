693ff872757cb9b579b7be19e83011daee9142af
rafaskb <rafa.skoberg@hotmail.com>
2019-08-30 15:33:49 -0300

    Major improvements to MathUtil and TableUtil, mostly documentation.

--- src/ReplicatedStorage/Aero/Shared/MathUtil.lua
+++ src/ReplicatedStorage/Aero/Shared/MathUtil.lua
@@ -1,16 +1,12 @@
---[[
-
-	MathUtil.Clamp(value, min, max)
-	MathUtil.NextTriangular(min, max, around)
-	MathUtil.Lerp(from, to, progress)
-	MathUtil.Round(value, bracket, trimDecimals)
-
---]]
-
+---
+---Utility class to help with math operations.
+---@class MathUtil
+---
 local MathUtil = {}
 
 ---
----Linearly normalizes value from a range. Range must not be empty. This is the inverse of lerp. (e.g. a value of 20 with ranges from 10 to 30 will return 0.5)
+---Linearly normalizes value from a range. Range must not be empty. This is the inverse of lerp.
+---Example: function(20, 10, 30) returns 0.5.
 ---@param value number Value to normalize.
 ---@param rangeStart number Range start normalized to 0.
 ---@param rangeEnd number Range end normalized to 1.
@@ -27,6 +23,7 @@ end
 
 ---
 ---Linearly map a value from one range to another. Input range must not be empty. This is the same as chaining Normalize from input range and Lerp to output range.
+---Example: function(20, 10, 30, 50, 100) returns 75.
 ---@param value number Value to map
 ---@param inRangeStart number Input range start
 ---@param inRangeEnd number Input range end
@@ -43,52 +40,78 @@ function MathUtil:Map(value, inRangeStart, inRangeEnd, outRangeStart, outRangeEn
     return result
 end
 
--- Clamps a value between minimum and maximum boundaries.
-local function Clamp(value, min, max)
-    return math.min(max, math.max(min, value))
+---
+---Returns a pseudorandom integer uniformly distributed over [min, max].
+---@param min number
+---@param max number
+---@return number
+---
+function MathUtil:NextNumber(min, max)
+    return self:Lerp(min, max, math.random())
 end
 
--- Returns a triangularly distributed random number between "min" and "max", where values close to the "around" one
--- are more likely to be chosen. In the event that "around" is not between the given boundaries, the result is clamped.
--- @param min The lower limit.
--- @param max The upper limit.
--- @param around The point around which the values are more likely.
-local function NextTriangular(min, max, around)
+---
+---Returns a pseudorandom integer uniformly distributed over [min, max].
+---@param min number
+---@param max number
+---@return number
+---
+function MathUtil:NextInteger(min, max)
+    return self:Round(self:Lerp(min, max, math.random()), 1)
+end
+
+---
+---Returns a triangularly distributed random number between "min" and "max", where values close to the "around" one
+---are more likely to be chosen. In the event that "around" is not between the given boundaries, the result is clamped.
+---@param min number The lower limit.
+---@param max number The upper limit.
+---@param around number The point around which the values are more likely.
+---@return number
+---
+function MathUtil:NextTriangular(min, max, around)
     local u = math.random()
     local d = max - min
+    local r = 0
     if u <= (around - min) / d then
-        return min + math.sqrt(u * d * (around - min))
+        r = min + math.sqrt(u * d * (around - min))
+    else
+        r = max - math.sqrt((1 - u) * d * (max - around))
     end
-    local r = max - math.sqrt((1 - u) * d * (max - around))
-    return Clamp(r, min, max);
+    return math.clamp(r, min, max)
 end
 
--- Returns a linear interpolation between the given boundaries at a certain progress.
-local function Lerp(from, to, progress)
+---
+---Returns a linear interpolation between the given boundaries at a certain progress.
+---@param from number
+---@param to number
+---@param progress number From 0 to 1.
+---@return number
+---
+function MathUtil:Lerp(from, to, progress)
     return from + (to - from) * progress
 end
 
---- Rounds a number according to the given bracket.
 ---
---- Examples:
----     Round(123.456789, 100)   -- 100
----     Round(123.456789, 10)    -- 120
----     Round(123.456789, 1)     -- 123
----     Round(123.456789, 0.1)   -- 123.5
----     Round(123.456789, 0.01)  -- 123.46
----     Round(123.456789, 0.001) -- 123.457
+---Rounds a number according to the given bracket.
 ---
-local function Round(value, bracket)
+---Examples:
+---    Round(123.456789, 100)   -- 100
+---    Round(123.456789, 10)    -- 120
+---    Round(123.456789, 1)     -- 123
+---    Round(123.456789, 0.1)   -- 123.5
+---    Round(123.456789, 0.01)  -- 123.46
+---    Round(123.456789, 0.001) -- 123.457
+---
+---@param value number
+---@param bracket number
+---@return number
+---
+function MathUtil:Round(value, bracket)
     bracket = bracket or 1
     if bracket == 0 then
         bracket = 1
     end
     return math.floor(value / bracket + 0.5) * bracket
 end
-
-MathUtil.Clamp = Clamp
-MathUtil.NextTriangular = NextTriangular
-MathUtil.Lerp = Lerp
-MathUtil.Round = Round
 
 return MathUtil
--- src/ReplicatedStorage/Aero/Shared/TableUtil.lua
+++ src/ReplicatedStorage/Aero/Shared/TableUtil.lua
@@ -1,419 +1,324 @@
--- Table Util
--- Crazyman32
--- September 13, 2017
-
---[[
-	
-	TableUtil.Copy(Table tbl)
-	TableUtil.Sync(Table tbl, Table templateTbl)
-	TableUtil.Print(Table tbl, String label, Boolean deepPrint)
-	TableUtil.FastRemove(Table tbl, Number index)
-	TableUtil.FastRemoveFirstValue(Table tbl, Variant value)
-	TableUtil.Map(Table tbl, Function callback)
-	TableUtil.Filter(Table tbl, Function callback)
-	TableUtil.Reduce(Table tbl, Function callback [, Number initialValue])
-	TableUtil.IndexOf(Table tbl, Variant item)
-	TableUtil.RemoveIndexOf(tbl, element) - Calls IndexOf and removes the table index with FastRemove.
-	TableUtil.Reverse(Table tbl)
-	TableUtil.Shuffle(Table tbl)
-	TableUtil.IsEmpty(Table tbl)
-	TableUtil.EncodeJSON(Table tbl)
-	TableUtil.DecodeJSON(String json)
-
-	EXAMPLES:
-
-		Copy:
-
-			Performs a deep copy of the given table.
-
-			local tbl = {"a", "b", "c"}
-			local tblCopy = TableUtil.Copy(tbl)
-
-
-		Sync:
-
-			Synchronizes a table to a template table. If the table does not have an
-			item that exists within the template, it gets added. If the table has
-			something that the template does not have, it gets removed.
-
-			local tbl1 = {kills = 0; deaths = 0; points = 0}
-			local tbl2 = {points = 0}
-			TableUtil.Sync(tbl2, tbl1)  -- In words: "Synchronize table2 to table1"
-			print(tbl2.deaths)
-
-
-		Print:
-
-			Prints out the table to the output in an easy-to-read format. Good for
-			debugging tables. If deep printing, avoid cyclical references.
-
-			local tbl = {a = 32; b = 64; c = 128; d = {x = 0; y = 1; z = 2}}
-			TableUtil.Print(tbl, "My Table", true)
-
-
-		FastRemove:
-
-			Removes an item from an array at a given index. Only use this if you do
-			NOT care about the order of your array. This works by simply popping the
-			last item in the array and overwriting the given index with the last
-			item. This is O(1), compared to table.remove's O(n) speed.
-
-			local tbl = {"hello", "there", "this", "is", "a", "test"}
-			TableUtil.FastRemove(tbl, 2)   -- Remove "there" in the array
-			print(table.concat(tbl, " "))  -- > hello this is a test
-
-
-		FastRemoveFirstValue:
-
-			Calls FastRemove on the first index that holds the given value.
-
-			local tbl = {"abc", "hello", "hi", "goodbye", "hello", "hey"}
-			local removed, atIndex = TableUtil.FastRemoveFirstValue(tbl, "hello")
-			if (removed) then
-				print("Removed at index " .. atIndex)
-				print(table.concat(tbl, " "))  -- > abc hi goodbye hello hey
-			else
-				print("Did not find value")
-			end
-
-		
-		Map:
-
-			This allows you to construct a new table by calling the given function
-			on each item in the table.
-
-			local peopleData = {
-				{firstName = "Bob"; lastName = "Smith"};
-				{firstName = "John"; lastName = "Doe"};
-				{firstName = "Jane"; lastName = "Doe"};
-			}
-
-			local people = TableUtil.Map(peopleData, function(item)
-				return {Name = item.firstName .. " " .. item.lastName}
-			end)
-
-			-- 'people' is now an array that looks like: { {Name = "Bob Smith"}; ... }
-
-
-		Filter:
-
-			This allows you to create a table based on the given table and a filter
-			function. If the function returns 'true', the item remains in the new
-			table; if the function returns 'false', the item is discluded from the
-			new table.
-
-			local people = {
-				{Name = "Bob Smith"; Age = 42};
-				{Name = "John Doe"; Age = 34};
-				{Name = "Jane Doe"; Age = 37};
-			}
-
-			local peopleUnderForty = TableUtil.Filter(people, function(item)
-				return item.Age < 40
-			end)
-
-
-		Reduce:
-
-			This allows you to reduce an array to a single value. Useful for quickly
-			summing up an array.
-
-			local tbl = {40, 32, 9, 5, 44}
-			local tblSum = TableUtil.Reduce(tbl, function(accumulator, value)
-				return accumulator + value
-			end)
-			print(tblSum)  -- > 130
-
-
-		IndexOf:
-
-			Returns the index of the given item in the table. If not found, this
-			will return nil.
-
-			local tbl = {"Hello", 32, true, "abc"}
-			local abcIndex = TableUtil.IndexOf("abc")     -- > 4
-			local helloIndex = TableUtil.IndexOf("Hello") -- > 1
-			local numberIndex = TableUtil.IndexOf(64)     -- > nil
-
-
-		Reverse:
-
-			Creates a reversed version of the array. Note: This is a shallow
-			copy, so existing references will remain within the new table.
-
-			local tbl = {2, 4, 6, 8}
-			local rblReversed = TableUtil.Reverse(tbl)  -- > {8, 6, 4, 2}
-
-
-		Shuffle:
-
-			Shuffles (i.e. randomizes) an array. This uses the Fisher-Yates algorithm.
-
-			local tbl = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-			TableUtil.Shuffle(tbl)
-			print(table.concat(tbl, ", "))  -- e.g. > 3, 6, 9, 2, 8, 4, 1, 7, 5
-	
---]]
-
-
-
+---
+---Utility class to help with table operations.
+---@class TableUtil
+---
 local TableUtil = {}
 
-local http = game:GetService("HttpService")
+local http = game:GetService("HttpService") ---@type HttpService
 
-
-local function CopyTable(t)
-	assert(type(t) == "table", "First argument must be a table")
-	local tCopy = {}
-	for k,v in pairs(t) do
-		if (type(v) == "table") then
-			tCopy[k] = CopyTable(v)
-		else
-			tCopy[k] = v
-		end
-	end
-	return tCopy
+---
+---Performs a deep copy of the given table and all its values.
+---@generic T : table
+---@param t T
+---@return T
+---
+function TableUtil:DeepCopy(t)
+    assert(type(t) == "table", "First argument must be a table")
+    local tCopy = {}
+    for k, v in pairs(t) do
+        if (type(v) == "table") then
+            tCopy[k] = self:DeepCopy(v)
+        else
+            tCopy[k] = v
+        end
+    end
+    return tCopy
 end
 
-
-local function Sync(tbl, templateTbl)
-
-	assert(type(tbl) == "table", "First argument must be a table")
-	assert(type(templateTbl) == "table", "Second argument must be a table")
-	
-	-- If 'tbl' has something 'templateTbl' doesn't, then remove it from 'tbl'
-	-- If 'tbl' has something of a different type than 'templateTbl', copy from 'templateTbl'
-	-- If 'templateTbl' has something 'tbl' doesn't, then add it to 'tbl'
-	for k,v in pairs(tbl) do
-		
-		local vTemplate = templateTbl[k]
-		
-		-- Remove keys not within template:
-		if (vTemplate == nil) then
-			tbl[k] = nil
-			
-		-- Synchronize data types:
-		elseif (type(v) ~= type(vTemplate)) then
-			if (type(vTemplate) == "table") then
-				tbl[k] = CopyTable(vTemplate)
-			else
-				tbl[k] = vTemplate
-			end
-		
-		-- Synchronize sub-tables:
-		elseif (type(v) == "table") then
-			Sync(v, vTemplate)
-		end
-		
-	end
-	
-	-- Add any missing keys:
-	for k,vTemplate in pairs(templateTbl) do
-		
-		local v = tbl[k]
-		
-		if (v == nil) then
-			if (type(vTemplate) == "table") then
-				tbl[k] = CopyTable(vTemplate)
-			else
-				tbl[k] = vTemplate
-			end
-		end
-		
-	end
-	
+---
+---Performs a shallow copy of the given table, simply passing its values around.
+---@generic T : table<any,any>
+---@param t T
+---@return T
+---
+function TableUtil:ShallowCopy(t)
+    assert(type(t) == "table", "First argument must be a table")
+    local tCopy = {}
+    for k, v in pairs(t) do
+        tCopy[k] = v
+    end
+    return tCopy
 end
 
+---
+---Synchronizes a table to a template table. If the table does not have an item that exists within the template,
+---it gets added. If the table has something that the template does not have, it gets removed.
+---@param tbl table<any,any>
+---@param templateTbl table<any,any>
+---
+function TableUtil:Sync(tbl, templateTbl)
+    assert(type(tbl) == "table", "First argument must be a table")
+    assert(type(templateTbl) == "table", "Second argument must be a table")
 
-local function FastRemove(t, i)
-	local n = #t
-	t[i] = t[n]
-	t[n] = nil
+    -- If 'tbl' has something 'templateTbl' doesn't, then remove it from 'tbl'
+    -- If 'tbl' has something of a different type than 'templateTbl', copy from 'templateTbl'
+    -- If 'templateTbl' has something 'tbl' doesn't, then add it to 'tbl'
+    for k, v in pairs(tbl) do
+        local vTemplate = templateTbl[k]
+
+        -- Remove keys not within template:
+        if (vTemplate == nil) then
+            tbl[k] = nil
+
+            -- Synchronize data types:
+        elseif (type(v) ~= type(vTemplate)) then
+            if (type(vTemplate) == "table") then
+                tbl[k] = self:DeepCopy(vTemplate)
+            else
+                tbl[k] = vTemplate
+            end
+
+            -- Synchronize sub-tables:
+        elseif (type(v) == "table") then
+            self:Sync(v, vTemplate)
+        end
+    end
+
+    -- Add any missing keys:
+    for k, vTemplate in pairs(templateTbl) do
+        local v = tbl[k]
+        if (v == nil) then
+            if (type(vTemplate) == "table") then
+                tbl[k] = self:DeepCopy(vTemplate)
+            else
+                tbl[k] = vTemplate
+            end
+        end
+    end
 end
 
-
-local function Map(t, f)
-	assert(type(t) == "table", "First argument must be a table")
-	assert(type(f) == "function", "Second argument must be an array")
-	local newT = {}
-	for k,v in pairs(t) do
-		newT[k] = f(v, k, t)
-	end
-	return newT
+---
+---Removes an item from the given array by replacing its index with the last element to avoid expensive table reordering. Only use this if you do NOT care about the order of your array.
+---@generic T : any
+---@param t T[]
+---@param i number
+---@return T
+---
+function TableUtil:FastRemove(t, i)
+    local n = #t
+    t[i] = t[n]
+    t[n] = nil
+    return n
 end
 
-
-local function Filter(t, f)
-	assert(type(t) == "table", "First argument must be a table")
-	assert(type(f) == "function", "Second argument must be an array")
-	local newT = {}
-	if (#t > 0) then
-		local n = 0
-		for i = 1,#t do
-			local v = t[i]
-			if (f(v, i, t)) then
-				n = (n + 1)
-				newT[n] = v
-			end
-		end
-	else
-		for k,v in pairs(t) do
-			if (f(v, k, t)) then
-				newT[k] = v
-			end
-		end
-	end
-	return newT
+---
+---Removes an item from the given array by replacing its index with the last element to avoid expensive table reordering. Only use this if you do NOT care about the order of your array.
+---@generic T : any
+---@param t T[]
+---@param element T
+---@return boolean,number Success flag and the element index.
+---
+function TableUtil:FastRemoveIndexOf(t, element)
+    local i = self:IndexOf(t, element)
+    if i then
+        self:FastRemove(t, i)
+        return true, i
+    end
+    return false, -1
 end
 
-
-local function Reduce(t, f, init)
-	assert(type(t) == "table", "First argument must be a table")
-	assert(type(f) == "function", "Second argument must be an array")
-	assert(init == nil or type(init) == "number", "Third argument must be a number or nil")
-	local result = (init or 0)
-	for k,v in pairs(t) do
-		result = f(result, v, k, t)
-	end
-	return result
+---
+---This allows you to construct a new table by calling the given function on each item in the table.
+---@generic T : any
+---@generic R : any
+---@param t T[]
+---@param f fun(T):R
+---@return R[]
+---
+function TableUtil:Map(t, f)
+    assert(type(t) == "table", "First argument must be a table")
+    assert(type(f) == "function", "Second argument must be a function")
+    local newT = {}
+    for k, v in pairs(t) do
+        newT[k] = f(v, k, t)
+    end
+    return newT
 end
 
-
-local function Print(tbl, label, deepPrint)
-
-	assert(type(tbl) == "table", "First argument must be a table")
-	assert(label == nil or type(label) == "string", "Second argument must be a string or nil")
-
-	label = (label or "TABLE")
-	
-	local strTbl = {}
-	local indent = " - "
-	
-	-- Insert(string, indentLevel)
-	local function Insert(s, l)
-		strTbl[#strTbl + 1] = (indent:rep(l) .. s .. "\n")
-	end
-	
-	local function AlphaKeySort(a, b)
-		return (tostring(a.k) < tostring(b.k))
-	end
-	
-	local function PrintTable(t, lvl, lbl)
-		Insert(lbl .. ":", lvl - 1)
-		local nonTbls = {}
-		local tbls = {}
-		local keySpaces = 0
-		for k,v in pairs(t) do
-			if (type(v) == "table") then
-				table.insert(tbls, {k = k, v = v})
-			else
-				table.insert(nonTbls, {k = k, v = "[" .. typeof(v) .. "] " .. tostring(v)})
-			end
-			local spaces = #tostring(k) + 1
-			if (spaces > keySpaces) then
-				keySpaces = spaces
-			end
-		end
-		table.sort(nonTbls, AlphaKeySort)
-		table.sort(tbls, AlphaKeySort)
-		for _,v in pairs(nonTbls) do
-			Insert(tostring(v.k) .. ":" .. (" "):rep(keySpaces - #tostring(v.k)) .. v.v, lvl)
-		end
-		if (deepPrint) then
-			for _,v in pairs(tbls) do
-				PrintTable(v.v, lvl + 1, tostring(v.k) .. (" "):rep(keySpaces - #tostring(v.k)) .. " [Table]")
-			end
-		else
-			for _,v in pairs(tbls) do
-				Insert(tostring(v.k) .. ":" .. (" "):rep(keySpaces - #tostring(v.k)) .. "[Table]", lvl)
-			end
-		end
-	end
-	
-	PrintTable(tbl, 1, label)
-	
-	print(table.concat(strTbl, ""))
-	
+---
+---This allows you to create a table based on the given table and a filter
+---function. If the function returns 'true', the item remains in the new
+---table; if the function returns 'false', the item is discluded from the
+---new table.
+---@generic T : any
+---@param t T[]
+---@param f fun(element:T):boolean
+---@return T[]
+---
+function TableUtil:Filter(t, f)
+    assert(type(t) == "table", "First argument must be a table")
+    assert(type(f) == "function", "Second argument must be a function")
+    local newT = {}
+    if (#t > 0) then
+        local n = 0
+        for i = 1, #t do
+            local v = t[i]
+            if (f(v, i, t)) then
+                n = (n + 1)
+                newT[n] = v
+            end
+        end
+    else
+        for k, v in pairs(t) do
+            if (f(v, k, t)) then
+                newT[k] = v
+            end
+        end
+    end
+    return newT
 end
 
-
-local function IndexOf(tbl, item)
-	for i = 1,#tbl do
-		if (tbl[i] == item) then
-			return i
-		end
-	end
-	return nil
+---
+---This allows you to reduce an array to a single value. Useful for quickly summing up an array.
+---@generic T : any
+---@param t T[]
+---@param f fun(accumulator:number, value:T):number
+---@param init number|nil
+---@return number
+---
+function TableUtil:Reduce(t, f, init)
+    assert(type(t) == "table", "First argument must be a table")
+    assert(type(f) == "function", "Second argument must be a function")
+    assert(init == nil or type(init) == "number", "Third argument must be a number or nil")
+    local result = (init or 0)
+    for k, v in pairs(t) do
+        result = f(result, v, k, t)
+    end
+    return result
 end
 
+---
+---Prints out the table to the output in an easy-to-read format. Good for
+---debugging tables. If deep printing, avoid cyclical references.
+---@param tbl table
+---@param label string
+---@param deepPrint boolean
+---
+function TableUtil:Print(tbl, label, deepPrint)
+    assert(type(tbl) == "table", "First argument must be a table")
+    assert(label == nil or type(label) == "string", "Second argument must be a string or nil")
 
-local function RemoveIndexOf(t, element)
-	local i = IndexOf(t, element)
-	if i then
-		FastRemove(t, i)
-	end
+    label = (label or "TABLE")
+
+    local strTbl = {}
+    local indent = " - "
+
+    -- Insert(string, indentLevel)
+    local function Insert(s, l)
+        strTbl[#strTbl + 1] = (indent:rep(l) .. s .. "\n")
+    end
+
+    local function AlphaKeySort(a, b)
+        return (tostring(a.k) < tostring(b.k))
+    end
+
+    local function PrintTable(t, lvl, lbl)
+        Insert(lbl .. ":", lvl - 1)
+        local nonTbls = {}
+        local tbls = {}
+        local keySpaces = 0
+        for k, v in pairs(t) do
+            if (type(v) == "table") then
+                table.insert(tbls, { k = k, v = v })
+            else
+                table.insert(nonTbls, { k = k, v = "[" .. typeof(v) .. "] " .. tostring(v) })
+            end
+            local spaces = #tostring(k) + 1
+            if (spaces > keySpaces) then
+                keySpaces = spaces
+            end
+        end
+        table.sort(nonTbls, AlphaKeySort)
+        table.sort(tbls, AlphaKeySort)
+        for _, v in pairs(nonTbls) do
+            Insert(tostring(v.k) .. ":" .. (" "):rep(keySpaces - #tostring(v.k)) .. v.v, lvl)
+        end
+        if (deepPrint) then
+            for _, v in pairs(tbls) do
+                PrintTable(v.v, lvl + 1, tostring(v.k) .. (" "):rep(keySpaces - #tostring(v.k)) .. " [Table]")
+            end
+        else
+            for _, v in pairs(tbls) do
+                Insert(tostring(v.k) .. ":" .. (" "):rep(keySpaces - #tostring(v.k)) .. "[Table]", lvl)
+            end
+        end
+    end
+
+    PrintTable(tbl, 1, label)
+
+    print(table.concat(strTbl, ""))
 end
 
-
-local function Reverse(tbl)
-	local tblRev = {}
-	local n = #tbl
-	for i = 1,n do
-		tblRev[i] = tbl[n - i + 1]
-	end
-	return tblRev
+---
+---Returns the index of the given item in the table. If not found, this will return nil.
+---@generic T : any
+---@param tbl T[]
+---@param item T
+---@return number
+---
+function TableUtil:IndexOf(tbl, item)
+    for i = 1, #tbl do
+        if (tbl[i] == item) then
+            return i
+        end
+    end
+    return nil
 end
 
-
-local function Shuffle(tbl)
-	assert(type(tbl) == "table", "First argument must be a table")
-	for i = #tbl, 2, -1 do
-		local j = math.random(i)
-		tbl[i], tbl[j] = tbl[j], tbl[i]
-	end
+---
+---Creates a reversed version of the array. Note: This is a shallow
+---copy, so existing references will remain within the new table.
+---@generic K : any
+---@generic V : any
+---@param tbl table<K,V>
+---@return table<K,V>
+---
+function TableUtil:Reverse(tbl)
+    local tblRev = {}
+    local n = #tbl
+    for i = 1, n do
+        tblRev[i] = tbl[n - i + 1]
+    end
+    return tblRev
 end
 
-
-local function IsEmpty(tbl)
-	return (next(tbl) == nil)
+---
+---Shuffles an array using the Fisher-Yates algorithm.
+---@param tbl table
+---
+function TableUtil:Shuffle(tbl)
+    assert(type(tbl) == "table", "First argument must be a table")
+    for i = #tbl, 2, -1 do
+        local j = math.random(i)
+        tbl[i], tbl[j] = tbl[j], tbl[i]
+    end
 end
 
-
-local function EncodeJSON(tbl)
-	return http:JSONEncode(tbl)
+---
+---Checks if the given table is empty.
+---@param tbl table
+---@return boolean
+---
+function TableUtil:IsEmpty(tbl)
+    return (next(tbl) == nil)
 end
 
-
-local function DecodeJSON(str)
-	return http:JSONDecode(str)
+---
+---Generate a JSON string from a Lua table.
+---@param tbl table
+---@return string
+---
+function TableUtil:EncodeJSON(tbl)
+    return http:JSONEncode(tbl)
 end
 
-
-local function FastRemoveFirstValue(t, v)
-	local index = IndexOf(t, v)
-	if (index) then
-		FastRemove(t, index)
-		return true, index
-	end
-	return false, nil
+---
+---Decodes a JSON string into a Lua table
+---@param str string
+---@return table
+---
+function TableUtil:DecodeJSON(str)
+    return http:JSONDecode(str)
 end
-
-
-TableUtil.Copy = CopyTable
-TableUtil.Sync = Sync
-TableUtil.FastRemove = FastRemove
-TableUtil.FastRemoveFirstValue = FastRemoveFirstValue
-TableUtil.Print = Print
-TableUtil.Map = Map
-TableUtil.Filter = Filter
-TableUtil.Reduce = Reduce
-TableUtil.IndexOf = IndexOf
-TableUtil.RemoveIndexOf = RemoveIndexOf
-TableUtil.Reverse = Reverse
-TableUtil.Shuffle = Shuffle
-TableUtil.IsEmpty = IsEmpty
-TableUtil.EncodeJSON = EncodeJSON
-TableUtil.DecodeJSON = DecodeJSON
-
 
 return TableUtil
