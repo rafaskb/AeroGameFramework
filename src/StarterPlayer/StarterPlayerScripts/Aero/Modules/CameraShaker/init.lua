d2a3db1045353a049cea5b8b8d2366ceddfc1e90
rafaskb <rafa.skoberg@hotmail.com>
2020-01-21 11:35:56 -0300

    Add StopSustained to CameraShaker

--- src/StarterPlayer/StarterPlayerScripts/Aero/Modules/CameraShaker/init.lua
+++ src/StarterPlayer/StarterPlayerScripts/Aero/Modules/CameraShaker/init.lua
@@ -1,61 +1,61 @@
 -- Camera Shaker
--- Crazyman32
+-- Stephen Leitnick
 -- February 26, 2018
 
 --[[
-	
+
 	CameraShaker.CameraShakeInstance
-	
+
 	cameraShaker = CameraShaker.new(renderPriority, callbackFunction)
-	
+
 	CameraShaker:Start()
 	CameraShaker:Stop()
+	CameraShaker:StopSustained([fadeOutTime])
 	CameraShaker:Shake(shakeInstance)
 	CameraShaker:ShakeSustain(shakeInstance)
 	CameraShaker:ShakeOnce(magnitude, roughness [, fadeInTime, fadeOutTime, posInfluence, rotInfluence])
 	CameraShaker:StartShake(magnitude, roughness [, fadeInTime, posInfluence, rotInfluence])
-	
-	
-	
+
+
+
 	EXAMPLE:
-	
+
 		local camShake = CameraShaker.new(Enum.RenderPriority.Camera.Value, function(shakeCFrame)
 			camera.CFrame = playerCFrame * shakeCFrame
 		end)
-		
+
 		camShake:Start()
-		
+
 		-- Explosion shake:
 		camShake:Shake(CameraShaker.Presets.Explosion)
-		
+
 		wait(1)
-		
+
 		-- Custom shake:
 		camShake:ShakeOnce(3, 1, 0.2, 1.5)
-	
-		wait(1)
- 		-- Sustained shake:
-		local swayShakeInstance = CameraShaker.Presets.GentleSway
-		camShake:ShakeSustain(swayShakeInstance)
 
-		wait(3)
- 		-- Sustained shake fadeout:
-		swayShakeInstance:StartFadeOut(3)
+		-- Sustained shake:
+		camShake:ShakeSustain(CameraShaker.Presets.Earthquake)
 
-		-- "CameraShaker.Presets.GentleSway" or any other preset
-		-- will always return a new ShakeInstance. If you want
-		-- to fade out a previously sustained ShakeInstance, you
-		-- will need to assign it to a variable before sustaining it.
+		-- Stop all sustained shakes:
+		camShake:StopSustained(1) -- Argument is the fadeout time (defaults to the same as fadein time if not supplied)
+
+		-- Stop only one sustained shake:
+		shakeInstance = camShake:ShakeSustain(CameraShaker.Presets.Earthquake)
+		wait(2)
+		shakeInstance:StartFadeOut(1) -- Argument is the fadeout time
 
 
 	NOTE:
-	
+
 		This was based entirely on the EZ Camera Shake asset for Unity3D. I was given written
 		permission by the developer, Road Turtle Games, to port this to Roblox.
-		
+
 		Original asset link: https://assetstore.unity.com/packages/tools/camera/ez-camera-shake-33148
-	
-	
+
+		GitHub repository: https://github.com/Sleitnick/RbxCameraShaker
+
+
 --]]
 
 
@@ -86,10 +86,10 @@ CameraShaker.Presets = require(script.CameraShakePresets)
 
 
 function CameraShaker.new(renderPriority, callback)
-	
+
 	assert(type(renderPriority) == "number", "RenderPriority must be a number (e.g.: Enum.RenderPriority.Camera.Value)")
 	assert(type(callback) == "function", "Callback must be a function")
-	
+
 	local self = setmetatable({
 		_running = false;
 		_renderName = "CameraShaker";
@@ -100,9 +100,9 @@ function CameraShaker.new(renderPriority, callback)
 		_removeInstances = {};
 		_callback = callback;
 	}, CameraShaker)
-	
+
 	return self
-	
+
 end
 
 
@@ -126,39 +126,49 @@ function CameraShaker:Stop()
 end
 
 
+function CameraShaker:StopSustained(duration)
+	for _,c in pairs(self._camShakeInstances) do
+		if (c.fadeOutDuration == 0) then
+			c:StartFadeOut(duration or c.fadeInDuration)
+		end
+	end
+end
+
+
 function CameraShaker:Update(dt)
-	
+
 	local posAddShake = v3Zero
 	local rotAddShake = v3Zero
-	
+
 	local instances = self._camShakeInstances
-	
+
 	-- Update all instances:
 	for i = 1,#instances do
-		
+
 		local c = instances[i]
 		local state = c:GetState()
-		
+
 		if (state == CameraShakeState.Inactive and c.DeleteOnInactive) then
 			self._removeInstances[#self._removeInstances + 1] = i
 		elseif (state ~= CameraShakeState.Inactive) then
-			posAddShake = posAddShake + (c:UpdateShake(dt) * c.PositionInfluence)
-			rotAddShake = rotAddShake + (c:UpdateShake(dt) * c.RotationInfluence)
+			local shake = c:UpdateShake(dt)
+			posAddShake = posAddShake + (shake * c.PositionInfluence)
+			rotAddShake = rotAddShake + (shake * c.RotationInfluence)
 		end
-		
+
 	end
-	
+
 	-- Remove dead instances:
 	for i = #self._removeInstances,1,-1 do
 		local instIndex = self._removeInstances[i]
 		table.remove(instances, instIndex)
 		self._removeInstances[i] = nil
 	end
-	
+
 	return CF(posAddShake) *
-			ANG(0, RAD(rotAddShake.Y), 0) *
-			ANG(RAD(rotAddShake.X), 0, RAD(rotAddShake.Z))
-	
+		ANG(0, RAD(rotAddShake.Y), 0) *
+		ANG(RAD(rotAddShake.X), 0, RAD(rotAddShake.Z))
+
 end
 
 
