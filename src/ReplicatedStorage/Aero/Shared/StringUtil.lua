91987a474be73125d7a82650e491ef3772fda155
Rafa Skoberg <rafa.skoberg@gmail.com>
2021-01-14 11:46:42 -0300

    Fix StringUtil:CommaValue not being able to process infinite numbers.

--- src/ReplicatedStorage/Aero/Shared/StringUtil.lua
+++ src/ReplicatedStorage/Aero/Shared/StringUtil.lua
@@ -57,6 +57,10 @@ local MathUtil ---@type MathUtil
 ---@return string
 ---
 function StringUtil:CommaValue(n)
+    if n == math.huge then
+        return "Infinite"
+    end
+
     -- credit http://richard.warburton.it
     local left, num, right = string.match(n, '^([^%d]*%d)(%d*)(.-)$')
     return left .. (num:reverse():gsub('(%d%d%d)', '%1,'):reverse()) .. right
